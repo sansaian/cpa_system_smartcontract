9a08adeb482ee24e68256cab294b23d53f05edeb
Suharik333 <suharik333@mail.ru>
2017-08-28 15:35:51 +0300

    small refactoring

--- cpa_system_smartcontract/cpaSystem.sol
+++ cpa_system_smartcontract/cpaSystem.sol
@@ -1,72 +1,79 @@
 pragma solidity ^0.4.0;
 contract Registrator  {
 
-    /*/
+    /*
      *  Contract fields
-    /*/
-    address private owner;
-    mapping (address => address) private dealStorage;
-    mapping (address => uint) private storeUsers;
-    address public cashierContract;
+     */
+     address private owner;
+     mapping (address => address) private dealsStorage;
+     mapping (address => uint) private usersStorage;
+     address public cashierContract;
 
-    /*/
+    /*
      *  Events
-    /*/
-    event LogetDealAddress(address _advertiser, address _smartDeal);
-    event LogstoreUser(address,uint);//role = 1 _advertiser role=2 webmaster
+     */
+     event LogGetDealAddress(address _advertiser, address _smartDeal);
+    event LogStoreUser(address,uint);//role = 1 _advertiser role=2 webmaster
     event LogResultCreateSmartDeal(address _advertiser, address smart_smartDealDeal);
     event LogAddress(address);
 
-    /*/
+    /*
      *  Сonstructor
-    /*/
-    function Registrator(){
+     */
+     function Registrator(){
         owner = msg.sender;
-        //create Casher
-        cashierContract = new Casher(owner);
+        //create Cashier
+        cashierContract = new Cashier(owner);
     }
 
-    /*/
+    /*
      *  Public functions
-    /*/
+     */
 
-    // @dev registers the user in the system
-    // @returns role user
-    // @param _user public key user
-    // @param _role the role of the user in the system
+   /*
+    * @dev registers the user in the system
+    * @returns role user
+    * @param _user public key user
+    * @param _role the role of the user in the system
+    *
+    */
     function registrationUser(address _user,uint _role) isOwner returns (uint) {
-        storeUsers[_user] = _role;
-        LogstoreUser(_user,  storeUsers[_user]);
-        return storeUsers[_user];
+        usersStorage[_user] = _role;
+        LogStoreUser(_user,  usersStorage[_user]);
+        return usersStorage[_user];
     }
 
+    /*
+     * @dev Returns address DealsmartContract,which was created by the advertiser
+     * if return value == 0x0 it means we have not inicialize deal
+     * @param _advertiser the public key of the advertiser who has launched an advertising campaign
+     */
 
-    // @dev Returns address DealsmartContract,which was created by the advertiser
-    // if return value == 0x0 it means we have not inicialize deal
-    // @param _advertiser the public key of the advertiser who has launched an advertising campaign
     function getAddress(address _advertiser) constant returns (address) {
-        LogetDealAddress(_advertiser,  dealStorage[_advertiser]);
-        return dealStorage[_advertiser];
+        LogGetDealAddress(_advertiser,  dealsStorage[_advertiser]);
+        return dealsStorage[_advertiser];
     }
 
-    // @dev creates a deal start advertising company
-    // @returns address of smart contract SmartDeal
-    // @param _advertiser - advertiser
-    // @param costClick - the price for the attracted client
+   /*
+    * @dev creates a deal start advertising company
+    * @returns address of smart contract SmartDeal
+    * @param _advertiser - advertiser
+    * @param costClick - the price for the attracted client
+    */
     function createSmartDeal(address _advertiser,uint costClick)isOwner returns (address) {
         if(cashierContract != 0x0){
-            dealStorage[_advertiser] = new SmartDeal(cashierContract,_advertiser ,costClick);
-      }
-        LogResultCreateSmartDeal( _advertiser,dealStorage[_advertiser]);
-        return dealStorage[_advertiser];
+            dealsStorage[_advertiser] = new SmartDeal(cashierContract,_advertiser ,costClick);
+        }
+        LogResultCreateSmartDeal( _advertiser,dealsStorage[_advertiser]);
+        return dealsStorage[_advertiser];
     }
 
-    /*/
+    /*
      *  Modifiers
-    /*/
-    modifier isOwner {
+     */
+     modifier isOwner {
         if (msg.sender == owner)
-        _;
+            _;
     }
 
 }
@@ -76,105 +83,113 @@ contract Registrator  {
 
 contract SmartDeal  {
 
-    /*/
+    /*
      *  Contract fields
-    /*/
-    address private owner;
-    uint public costforclick;
-    uint public countOfclick=0;
-    address public advertiser;
-    mapping (address => uint) private mappWebmastersClick;
-    string advertiserReview;
+     */
+     address private owner;
+     uint public costforclick;
+     uint public countOfclick=0;
+     address public advertiser;
+     mapping (address => uint) private mappWebmastersClick;
+     string advertiserReview;
 
-    /*/
+    /*
      *  Сonstructor
-    /*/
-
-    function SmartDeal(address _cashierContract, address _advertiser,uint _costClick){
+     */
+     function SmartDeal(address _cashierContract, address _advertiser,uint _costClick){
         advertiser = _advertiser;
         owner = _cashierContract;
         costforclick = _costClick;
     }
 
-    /*/
+    /*
      *  Events
-    /*/
+     */
+     event LogIsWriteReview(address, bool);
 
-    event LogIsWriteReview(address, bool);
-
-    /*/
+    /*
      *  Public functions
-    /*/
+     */
 
-    // @dev feedback from Advertiser
-    // @param review
+   /*
+    * @dev feedback from Advertiser
+    * @param review
+    *
+    */
     function  writeAdvertiserReview (string _review) isAdvertiser {
         advertiserReview = _review;
         LogIsWriteReview(advertiser, true);
     }
 
-    // @dev increase counter of attracted client
-    // @returns count of attracted client
-    // @param webmaster
+    /*
+     * @dev increase counter of attracted client
+     * @returns count of attracted client
+     * @param webmaster
+     *
+     */
     function increaseCountofClick(address webmaster) isOwner returns(uint){
         mappWebmastersClick[webmaster];
         countOfclick++;
         return countOfclick;
     }
-     modifier isOwner {
+    modifier isOwner {
         if (msg.sender == owner)
-        _;
+            _;
     }
-     modifier isAdvertiser {
+    modifier isAdvertiser {
         if (msg.sender == advertiser)
-        _;
+            _;
     }
 
 }
 
-contract Casher {
-    /*/
+contract Cashier {
+    /*
      *  Contract fields
-    /*/
-    address public owner;
-    address public  registratorAddress;
-    mapping (address => uint) private cashRegister;
+     */
+     address public owner;
+     address public  registratorAddress;
+     mapping (address => uint) private cashRegister;
 
-    /*/
+    /*
      *  Events
-    /*/
+     */
+     event LogClick(address _advertiser, bool result);
+     event LogBalance(address ,uint);
 
-    event LogClick(address _advertiser, bool result);
-    event LogBalance(address ,uint);
-
-    /*/
+    /*
      *  Сonstructor
-    /*/
+     */
 
-    function Casher(address _owner){
+     function Cashier(address _owner){
         registratorAddress=msg.sender;
         owner = _owner;
     }
 
-    /*/
+    /*
      *  Public functions
-    /*/
+     */
 
-    // @dev charge tokens advertiser
-    // @returns bool
-    // @param _user - who to charge
-    // @param _countToken how much to charge
+   /*
+    * @dev charge tokens advertiser
+    * @returns bool
+    * @param _user - who to charge
+    * @param _countToken how much to charge
+    */
     function chargeToken(address _user,uint _countToken) isOwner returns (bool){
         cashRegister[_user] +=_countToken;
         if(cashRegister[_user]==0)return false;
         return true;
     }
 
-    // @dev the fact that attracted customer
-    // @returns bool
-    // @param _advertiser - advertiser
-    // @param webMaster - webMaster
-    // @param costOfClick from order
+   /*
+    * @dev the fact that attracted customer
+    * @returns bool
+    * @param _advertiser - advertiser
+    * @param webMaster - webMaster
+    * @param costOfClick from order
+    *
+    */
     function doClick(address _advertiser,address _webMaster,uint _costOfClick) isOwner
     returns(bool){
         if (cashRegister[_advertiser] < _costOfClick){
@@ -187,11 +202,13 @@ contract Casher {
 
     }
 
-    // @dev increase number of referred customers in the contract SmartDeal
-    // @returns bool
-    // @param _advertiser - advertiser
-    // @param webMaster - webMaster
-    function  increaseCountofClick(address _advertiser,address _webMaster) private returns(bool){
+    /*
+     * @dev increase number of referred customers in the contract SmartDeal
+     * @returns bool
+     * @param _advertiser - advertiser
+     * @param webMaster - webMaster
+     */
+     function  increaseCountofClick(address _advertiser,address _webMaster) private returns(bool){
         Registrator registrator = Registrator(registratorAddress);
         address contractSmartDeal =registrator.getAddress(_advertiser);
         LogBalance(contractSmartDeal ,1);
@@ -199,20 +216,22 @@ contract Casher {
             SmartDeal smartDeal = SmartDeal(contractSmartDeal);
             uint countClick = smartDeal.increaseCountofClick(_webMaster);
             if(countClick>=0)
-            return true;
+                return true;
         }
         return false;
     }
-    // @dev check  user's balance
-    // @returns the number of coins
-    // @param _user
-    function balanceOf(address _user) constant returns (uint256) {
+    /*
+     * @dev check  user's balance
+     * @returns the number of coins
+     * @param _user
+     */
+     function balanceOf(address _user) constant returns (uint256) {
         LogBalance(_user,cashRegister[_user]);
         return cashRegister[_user];
     }
 
     modifier isOwner {
         if (msg.sender == owner)
-        _;
+            _;
     }
 }
